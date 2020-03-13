0ec1e01a462c5e69402a5b0822a19c77a2edba84
sunshiyao123 <805692794@qq.com>
2020-03-13 22:52:24 +0800

    2020-3-13

---
+++ 每日一题/Project/src/Day27/Main.java
@@ -0,0 +1,17 @@
+//package Day27;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int length = Integer.valueOf(scanner.nextLine());
+//        int[] data = new int[length];
+//        String s = scanner.nextLine();
+//        char[] data1 = new char[length];
+//        data1 = s.toCharArray();
+//        for (int i = 0;i < length;i++) {
+//            data[i] = data1[i]+""
+//        }
+//    }
+//}
