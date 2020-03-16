86fefd2720f4d55b06509a2cbe9abab0104f165f
sunshiyao123 <805692794@qq.com>
2020-03-16 23:30:07 +0800

    2020-3-16

---
+++ 每日一题/Project/src/Day37/Main.java
@@ -0,0 +1,10 @@
+package Day37;
+
+import java.util.Scanner;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int maxValue;
+//        System.out.println(maxValue);
+//    }
+//}
