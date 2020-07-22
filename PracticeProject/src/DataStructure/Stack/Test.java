6dd00181498ef9d397f1f33af7ba10b4ad62b925
sunshiyao123 <805692794@qq.com>
2020-07-22 23:59:32 +0800

    2020-7-22

---
+++ PracticeProject/src/DataStructure/Stack/Test.java
@@ -0,0 +1,13 @@
+package DataStructure.Stack;
+
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        String[] number = new String[]{"yi","er","san"};
+        for (String s : number) {
+            System.out.println(s);
+        }
+        StringBuilder[] data = new StringBuilder[5];
+    }
+}
