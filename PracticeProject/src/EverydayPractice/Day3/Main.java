e68b3d3e8e515cf7ce840242a588cb78513a920e
sunshiyao123 <805692794@qq.com>
2020-07-29 23:59:05 +0800

    2020-7-29

---
+++ PracticeProject/src/EverydayPractice/Day3/Main.java
@@ -0,0 +1,47 @@
+package EverydayPractice.Day3;
+/**
+ * Broken Keyboard
+ * 坏的键盘:
+ *
+ * 用的TreeMap
+ */
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        char[] data1 = scanner.nextLine().toCharArray();
+//        char[] temp = scanner.nextLine().toCharArray();
+//        char[] data2 = new char[data1.length];
+//        int x = 0;
+//        for(char c : temp) {
+//            data2[x++] = c;
+//        }
+//        Map<Character,Integer> map = new TreeMap<>();
+//        int i = 0;
+//        int j = 0;
+//        StringBuilder sb = new StringBuilder();
+//        while(i < data1.length) {
+//            if(data1[i] == (data2[j])) {
+//                i++;
+//                j++;
+//            } else if(data1[i] <= 'z' && data1[i] >= 'a') {
+//                //放入数据，判断是否已经有，如果重复则给数据+1；
+//                char temp1 = (char)(data1[i] -32);
+//                map.put(temp1,map.getOrDefault(temp1,0)+1);
+//                if(map.get(temp1) == 1)
+//                    sb.append(temp1);
+//                i++;
+//            } else {
+//                map.put(data1[i],map.getOrDefault(data1[i],0)+1);
+//                if(map.get(data1[i]) == 1)
+//                    sb.append(data1[i]);
+//                i++;
+//            }
+//        }
+//
+//        System.out.println(sb.toString());
+//        return;
+
+    }
+}
