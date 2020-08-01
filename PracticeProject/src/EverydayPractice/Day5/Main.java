d8c985b97f25f4d5f018c54ba99c058e6a88c9c2
sunshiyao123 <805692794@qq.com>
2020-08-01 22:53:26 +0800

    2020-8-1

---
+++ PracticeProject/src/EverydayPractice/Day5/Main.java
@@ -0,0 +1,11 @@
+package EverydayPractice.Day5;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new TreeMap<>();
+        map.put("事要",1);
+    }
+}
